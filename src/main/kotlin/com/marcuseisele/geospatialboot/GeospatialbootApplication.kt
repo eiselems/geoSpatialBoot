8dce4c551e331fa1b82d780b5b7b370994604533
eiselems <marcus.eisele@gmail.com>
2019-03-26 19:16:22 +0100

    INIT

---
+++ src/main/kotlin/com/marcuseisele/geospatialboot/GeospatialbootApplication.kt
@@ -0,0 +1,11 @@
+package com.marcuseisele.geospatialboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeospatialbootApplication
+
+fun main(args: Array<String>) {
+	runApplication<GeospatialbootApplication>(*args)
+}
